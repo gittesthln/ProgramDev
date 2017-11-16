fb5998f3ec1bdec36482b52cfb2010682d673af0
gittesthln <gittesthln@gmail.com>
2017-11-16 14:44:11 +0900

    add files for 9th

---
+++ 09testPHP.php
@@ -0,0 +1,21 @@
+<?php
+$A = 2;               //JavaScriptのときは $ は取り除く。
+$B = 5;
+
+print "1:$A+$B\n";    //JavaScriptのときは print は　console.log() になおす。
+print "2:"+$A+$B+"\n";
+print "3:".$A+$B."\n";//JavaScript のときは省略すること
+
+print sum(10,20,30)."\n";
+print sum(10,20)."\n";
+print sum(10,20,30,40)."\n";
+
+print sum2(10,20)."\n";
+
+function sum($a,$b,$c){
+  return $a+$b+$c;
+}
+function sum2($a,$b){
+  return $A+$B;
+}
+?>
