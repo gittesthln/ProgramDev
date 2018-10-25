6aa867b6fde310d0939330d05ee7095d931b3d76
gittesthln <gittesthln@gmail.com>
2018-10-25 14:29:44 +0900

    correct minor error

--- primes/countPrimes-Ajax.js
+++ primes/countPrimes-Ajax.js
@@ -12,15 +12,15 @@ function getData() {
   let table = document.getElementsByTagName("table")[0];
   let P = table.parentNode;
   P.removeChild(table);
-  table = MKHTMLElm(P, "table",{}, {});
+  table = MakeElement(P, "table");
   let request = 0, start = new Date();
   for(let i=0; i<10; i++) {
     let httpObj = createXMLHTTPReq(function(){
       if(httpObj.readyState == 4 && httpObj.status == 200) {
-        let tr = MKHTMLElm(table,"tr",{}, {});
-        MKHTMLElm(tr, "td", {}, {}).
+        let tr = MakeElement(table,"tr");
+        MakeElement(tr, "td").
             appendChild(document.createTextNode(i*interval+1));
-        MKHTMLElm(tr, "td", {}, {}).
+        MakeElement(tr, "td").
               appendChild(document.createTextNode(httpObj.responseText));
           primes -= -httpObj.responseText;
         request--;
@@ -35,5 +35,16 @@ function getData() {
                    window.Test["ajax-mode"].value == "true");
       httpObj.send(null);
     }
+  }
+}
+function MakeElement(P, elem, attribs, events) {
+  let Element = document.createElement(elem);
+  SetAttributes(Element, attribs);
+  if(P) P.appendChild(Element);
+  return Element;
+} 
+function SetAttributes(Elm, attribs) {
+  for( attrib in attribs) {
+      Elm.setAttribute(attrib,attribs[attrib]);
   }
 }
